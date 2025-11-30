0ee8de3beb9f1a8280dced5dc75ff63e6c639e32
LaurMaldis <117661481+LaurMaldis@users.noreply.github.com>
2025-11-30 19:02:45 +0200

    Update rke143.js

--- routes/rke143.js
+++ routes/rke143.js
@@ -5,10 +5,11 @@ const rke143Data = require('../data/nodejs.json');
 
 
 router.post('/', (req, res) => {
-    //name - rke
-    //code - 143
-
-    //invalid credentials
+    if(req.body.name !== 'rke' || req.body.code !== "143") {
+        res.status(200).json({message: 'Invalid credentials'}); 
+    } else {
+        res.status(200).json({rke143Data});
+    }
 });
 
 
